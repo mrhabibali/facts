11342cf75b42af58b6ea72781370500eed4e3b50
mrhabibali <habibali92@hotmail.com>
2023-02-15 17:39:06 +0100

    Added a class in the file

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,16 +2,16 @@ import "../Main.css";
 
 function Main() {
   return (
-    <div className="content">
+    <main>
       <h1 className="title">Facts about React</h1>
-      <ul>
+      <ul className="facts">
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100K stars on GitHub</li>
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
-    </div>
+    </main>
   );
 }
 
